fd58a55ffce19c7d2a8745d3b62fa905195594b1
Juhyeok Sin <homin203@gmail.com>
2018-04-02 11:37:34 -0700

    modification on indents with double spaces

--- Evaluation.java
+++ Evaluation.java
@@ -2,5 +2,5 @@ package sudoku;
 
 enum Evaluation
 {
-	ACCEPT, ABANDON, CONTINUE;
+  ACCEPT, ABANDON, CONTINUE;
 }
--- Grid.java
+++ Grid.java
@@ -5,221 +5,222 @@ import java.util.*;
 public class Grid {
 	// instance variable values
 	// values represents 9 x 9 grid
-	private int[][]	values;
+  private int[][]  values;
 
-	/**
+  /**
    * Constructs an object Grid
    * Transforms a string array into two dimensional int array]
-   * @param  rows [description]
+   * @param  rows string array
    */
-	public Grid(String[] rows) {
-		values = new int[9][9];
-		for (int j=0; j<9; j++) {
-			String row = rows[j];
-			char[] charray = row.toCharArray();
-			for (int i=0; i<9; i++) {
-				char ch = charray[i];
-				if (ch != '.')
-					values[j][i] = ch - '0';
-					// int value is assigned
-					// int value = '3' - '0';
-					// Then, the variable value stores 3 as its value
-			}
-		}
-	}
+  public Grid(String[] rows) {
+    // constructs two dimensional int array size of 9 x 9
+    values = new int[9][9];
+    for (int j=0; j<9; j++) {
+      String row = rows[j];
+      char[] charray = row.toCharArray();
+      for (int i=0; i<9; i++) {
+        char ch = charray[i];
+        if (ch != '.')
+          values[j][i] = ch - '0';
+          // int value is assigned
+          // int value = '3' - '0';
+          // Then, the variable value stores 3 as its value
+      }
+    }
+  }
 
 
-	public String toString() {
-		String s = "";
-		for (int j=0; j<9; j++) {
-			for (int i=0; i<9; i++) {
-				int n = values[j][i];
-				if (n == 0)
-					s += '.';
-				else
-					s += (char)('0' + n);
-			}
-			s += "\n";
-		}
-		return s;
-	}
+  public String toString() {
+    String s = "";
+    for (int j=0; j<9; j++) {
+      for (int i=0; i<9; i++) {
+        int n = values[j][i];
+        if (n == 0)
+          s += '.';
+        else
+          s += (char)('0' + n);
+      }
+      s += "\n";
+    }
+    return s;
+  }
 
 
-	//
-	// Copy ctor. Duplicates its source. call this 9 times in next9Grids.
-	//
-	Grid(Grid src) {
-		values = new int[9][9];
-		for (int j=0; j<9; j++)
-			for (int i=0; i<9; i++)
-				values[j][i] = src.values[j][i];
-	}
+  //
+  // Copy ctor. Duplicates its source. call this 9 times in next9Grids.
+  //
+  Grid(Grid src) {
+    values = new int[9][9];
+    for (int j=0; j<9; j++)
+      for (int i=0; i<9; i++)
+        values[j][i] = src.values[j][i];
+  }
 
 
-	//
-	//
-	//
-	// Finds an empty member of values[][].
-	// Returns an array list of 9 grids that look like the current grid,
-	// except the empty member contains 1, 2, 3 .... 9.
-	// Returns null if the current grid is full.
-	public ArrayList<Grid> next9Grids() {
-		int xOfNextEmptyCell = -1;
-		int yOfNextEmptyCell = -1;
+  //
+  //
+  //
+  // Finds an empty member of values[][].
+  // Returns an array list of 9 grids that look like the current grid,
+  // except the empty member contains 1, 2, 3 .... 9.
+  // Returns null if the current grid is full.
+  public ArrayList<Grid> next9Grids() {
+    int xOfNextEmptyCell = -1;
+    int yOfNextEmptyCell = -1;
 
-		// Find x,y of an empty cell.
-		if (this.isFull())
-			return null;
-		else {
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					if (this.values[i][j] == 0) {
-						xOfNextEmptyCell = i;
-						yOfNextEmptyCell = j;
-						i = 9;
-						j = 9;
-					}
-				}
-			}
-		}
-		// Construct array list to contain 9 new grids.
-		ArrayList<Grid> grids = new ArrayList<Grid>();
-		for (int i = 0; i < 9; i++) {
-			grids.add(new Grid(this));
-		}
-		// Create 9 new grids as described in the comments above. Add them to grids.
-		for (int i = 0; i < 9; i++) {
-			grids.get(i).values[xOfNextEmptyCell][yOfNextEmptyCell] = i + 1;
-		}
-		return grids;
-	}
-
-
-	/**
-	 * Checks every rows, columns, and blocks are legal
-	 * Row, column, or block is legal when there is no repeated number between 1 - 9
-	 * Note that the Object must have a two dimensional array instance named grid.
-	 */
-	public boolean isLegal() {
-		int count4row    = 0;
-		int count4column = 0;
-		int count4block  = 0;
-		// Check every row. If you find an illegal row, return false.
-		int[] ithRow = new int[9];
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				ithRow[j] = values[i][j];
-			}
-			if (containsNonZeroRepeat(ithRow) == true)
-				count4row++;
-		}
-		// Check every column. If you find an illegal column, return false.
-		int[] ithColumn = new int[9];
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				ithColumn[j] = values[j][i];
-			}
-			if (containsNonZeroRepeat(ithColumn) == true)
-				count4column++;
-		}
-		// Check every block. If you find an illegal block, return false.
-		int[] block = new int[9];
-		for (int i = 3; i <= 9; i = i + 3) {
-			for (int j = 3; j <= 9; j = j + 3) {
-				block = nthBlockToLinear(i, j);
-				if (containsNonZeroRepeat(block) == true)
-					count4block++;
-			}
-		}
-		// All rows/columns/blocks are legal.
-		return ((count4row == 9) && (count4column == 9) && (count4block == 9)) ? true : false;
-	}
+    // Find x,y of an empty cell.
+    if (this.isFull())
+      return null;
+    else {
+      for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+          if (this.values[i][j] == 0) {
+            xOfNextEmptyCell = i;
+            yOfNextEmptyCell = j;
+            i = 9;
+            j = 9;
+          }
+        }
+      }
+    }
+    // Construct array list to contain 9 new grids.
+    ArrayList<Grid> grids = new ArrayList<Grid>();
+    for (int i = 0; i < 9; i++) {
+      grids.add(new Grid(this));
+    }
+    // Create 9 new grids as described in the comments above. Add them to grids.
+    for (int i = 0; i < 9; i++) {
+      grids.get(i).values[xOfNextEmptyCell][yOfNextEmptyCell] = i + 1;
+    }
+    return grids;
+  }
 
 
-	/**
-	 * linearizes a two dimensional array that is 3 x 3 grid into an one dimensional array
-	 * ---
-	 * Transform a two dimensional grid into a linear array.
-	 * Note that the Object must have a two dimensional array instance named values.
-	 * ---
-	 * @param  offsetRow offset of row to extract from the values
-	 * @param  offsetCol offset of column to extract from the values
-	 * @return           a linear array
-	 */
-	public int[] TwoDArrayToLinear(int offsetRow, int offsetCol) {
-		// declare an array linearized, size of 9
-		int[] linearArray = new int[9];
-		int index = 0;
-
-		for (int row = offsetRow - 3; row < offsetRow; row++) {
-			for (int col = offsetCol - 3; col < offsetCol; col++) {
-				// copy the value at [row][col] position
-				// to the linearArray
-				linearArray[index] = this.values[row][col];
-				index++;
-			}
-		}
-		return linearArray;
-	}
-
-	// cost can be saved by using Set
-	public boolean containsNonZeroRepeat(int[] value) {
-		// int count = 0;
-		for (int i = 0; i < 8; i++) {
-			for (int j = i + 1; j < 9; j++) {
-				if (value[i] == value[j] && value[i] > 0 && value[j] > 0)
-				return false;
-			}
-		}
-		return true;
-	}
-
-	//
-	//
-	// Returns true if every cell member of values[][] is a digit from 1-9.
-	//
-	public boolean isFull() {
-		boolean isFull = false;
-		for(int i = 0; i < 9; i++) {
-			if (values[i][0] < 1 || values[i][0] > 9)
-				return false;
-			for(int j = 0; j < 9; j++) {
-				if (values[i][j] < 1 || values[i][j] > 9)
-					return false;
-			}
-		}
-		return true;
-	}
+  /**
+   * Checks every rows, columns, and blocks are legal
+   * Row, column, or block is legal when there is no repeated number between 1 - 9
+   * Note that the Object must have a two dimensional array instance named grid.
+   */
+  public boolean isLegal() {
+    int count4row    = 0;
+    int count4column = 0;
+    int count4block  = 0;
+    // Check every row. If you find an illegal row, return false.
+    int[] ithRow = new int[9];
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        ithRow[j] = values[i][j];
+      }
+      if (containsNonZeroRepeat(ithRow) == true)
+        count4row++;
+    }
+    // Check every column. If you find an illegal column, return false.
+    int[] ithColumn = new int[9];
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        ithColumn[j] = values[j][i];
+      }
+      if (containsNonZeroRepeat(ithColumn) == true)
+        count4column++;
+    }
+    // Check every block. If you find an illegal block, return false.
+    int[] block = new int[9];
+    for (int i = 3; i <= 9; i = i + 3) {
+      for (int j = 3; j <= 9; j = j + 3) {
+        block = nthBlockToLinear(i, j);
+        if (containsNonZeroRepeat(block) == true)
+          count4block++;
+      }
+    }
+    // All rows/columns/blocks are legal.
+    return ((count4row == 9) && (count4column == 9) && (count4block == 9)) ? true : false;
+  }
 
 
-	//
-	//
-	// Returns true if x is a Grid and, for every (i,j),
-	// x.values[i][j] == this.values[i][j].
-	//
-	public boolean equals(Object x) {
-		boolean equiv = false;
-		int countEqual = 0;
-		Grid that = (Grid)x;
+  /**
+   * linearizes a two dimensional array that is 3 x 3 grid into an one dimensional array
+   * ---
+   * Transform a two dimensional grid into a linear array.
+   * Note that the Object must have a two dimensional array instance named values.
+   * ---
+   * @param  offsetRow offset of row to extract from the values
+   * @param  offsetCol offset of column to extract from the values
+   * @return           a linear array
+   */
+  public int[] TwoDArrayToLinear(int offsetRow, int offsetCol) {
+    // declare an array linearized, size of 9
+    int[] linearArray = new int[9];
+    int index = 0;
 
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (this.values[i][j] == that.values[i][j])
-					countEqual++;
-			}
-		}
-		if (countEqual == 81)
-			equiv = true;
-		return equiv;
-	}
+    for (int row = offsetRow - 3; row < offsetRow; row++) {
+      for (int col = offsetCol - 3; col < offsetCol; col++) {
+        // copy the value at [row][col] position
+        // to the linearArray
+        linearArray[index] = this.values[row][col];
+        index++;
+      }
+    }
+    return linearArray;
+  }
 
-	/**
-	 * main method to test the current class Grid
-	 */
-	public static void main(String[] args) {
-		Grid grid1 = new Grid(TestGridSupplier.getSolution1());
-		Grid grid2 = new Grid(TestGridSupplier.getPuzzle2());
-		System.out.println("Is grid1 full? " + grid1.isFull());
-		System.out.println("Is grid2 full? " + grid2.isFull());
-	}
+  // cost can be saved by using Set
+  public boolean containsNonZeroRepeat(int[] value) {
+    // int count = 0;
+    for (int i = 0; i < 8; i++) {
+      for (int j = i + 1; j < 9; j++) {
+        if (value[i] == value[j] && value[i] > 0 && value[j] > 0)
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //
+  //
+  // Returns true if every cell member of values[][] is a digit from 1-9.
+  //
+  public boolean isFull() {
+    boolean isFull = false;
+    for(int i = 0; i < 9; i++) {
+      if (values[i][0] < 1 || values[i][0] > 9)
+        return false;
+      for(int j = 0; j < 9; j++) {
+        if (values[i][j] < 1 || values[i][j] > 9)
+          return false;
+      }
+    }
+    return true;
+  }
+
+
+  //
+  //
+  // Returns true if x is a Grid and, for every (i,j),
+  // x.values[i][j] == this.values[i][j].
+  //
+  public boolean equals(Object x) {
+    boolean equiv = false;
+    int countEqual = 0;
+    Grid that = (Grid)x;
+
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (this.values[i][j] == that.values[i][j])
+          countEqual++;
+      }
+    }
+    if (countEqual == 81)
+      equiv = true;
+    return equiv;
+  }
+
+  /**
+   * main method to test the current class Grid
+   */
+  public static void main(String[] args) {
+    Grid grid1 = new Grid(TestGridSupplier.getSolution1());
+    Grid grid2 = new Grid(TestGridSupplier.getPuzzle2());
+    System.out.println("Is grid1 full? " + grid1.isFull());
+    System.out.println("Is grid2 full? " + grid2.isFull());
+  }
 }
